2870fa7757ca8bae3467a37924cd76dca621789e
shashikalaayeshani86 <shashikalaayeshani86@gmail.com>
2025-05-07 12:51:35 +0530

    Before deploying6

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Required to listen inside Docker
-    port: 5173       // Must match the internal port in docker
+    port: 5173 ,      // Must match the internal port in docker
+    base: "/Portfolio/",
   }
 })
